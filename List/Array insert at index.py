33a695cc6d97320af8cc29d96bc343d5ab83580c
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-06-05 11:18:14 +0530

    Create Array insert at index.py

---
+++ List/Array insert at index.py
@@ -0,0 +1,40 @@
+Link : https://practice.geeksforgeeks.org/batch/ds-with-python/track/list-basic-python/problem/array-insert-at-index
+  
+#User function Template for python3
+
+class Solution:
+    '''You need to insert the given element at the given index. 
+    After inserting the elements at index, elements
+    from index onward should be shifted one position ahead
+     You may assume that the array already has sizeOfArray - 1
+    elements.'''
+    
+    def insertAtIndex(self, arr, sizeOfArray, index, element):
+        #Your code here
+        arr.insert(index,element)
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        sizeOfArray=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        arr.append(-1)
+        index,element=map(int,input().strip().split())
+        
+        Solution().insertAtIndex(arr,sizeOfArray,index,element)
+        
+        for i in range(sizeOfArray):
+            print(arr[i],end=" ")
+        print()
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
