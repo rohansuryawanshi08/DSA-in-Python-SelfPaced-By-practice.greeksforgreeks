1bdaefdddd39ef6f2aff62dc08652e05f470d76f
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-06-14 15:31:39 +0530

    Create Peak element.py

---
+++ Searching/Peak element.py
@@ -0,0 +1,46 @@
+Link : https://practice.geeksforgeeks.org/batch/ds-with-python/track/searching-basic-python/problem/peak-element
+    
+# your task is to complete this function
+# function should return index to the any valid peak element
+class Solution:   
+    def peakElement(self,arr, n):
+        # Code here
+        
+        maxi=max(arr)
+        for i in range(0,len(arr)):
+            
+            if arr[i]==maxi:
+                return i
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        index = Solution().peakElement(arr.copy(), n)
+        flag = False
+        if index<0 or index>=n:
+            flag = False
+        else:
+            if index == 0 and n==1:
+                flag = True
+            elif index==0 and arr[index]>=arr[index+1]:
+                flag = True
+            elif index==n-1 and arr[index]>=arr[index-1]:
+                flag = True
+            elif arr[index-1] <= arr[index] and arr[index] >= arr[index+1]:
+                flag = True
+            else:
+                flag = False
+
+        if flag:
+            print(1)
+        else:
+            print(0)
+# Contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
